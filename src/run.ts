a67c6645ca6c2e59ff4a7f33f56a5c63fb3a1cf7
Grundy <urffin2006@gmail.com>
2023-02-09 19:25:34 +0300

    add solution

--- src/run.ts
+++ src/run.ts
@@ -3,8 +3,49 @@ import ITask from './Task';
 
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
-    /**
-     * Код надо писать сюда
-     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
-     */
+
+    if (maxThreads == 0) {
+        const mapQueue = new Map<number, Promise<void>>();
+        for await (const task of queue) {
+            const promise = mapQueue.get(task.targetId);
+            if (!promise) {
+                mapQueue.set(task.targetId, executor.executeTask(task));
+                continue;
+            }
+
+            mapQueue.set(task.targetId, promise.then(() => executor.executeTask(task)));
+        }
+        return Promise.all(mapQueue.values())
+    }
+
+    const q: Promise<void>[] = Array(maxThreads).fill(Promise.resolve());
+    // const qq: number[] = Array(maxThreads).fill(-1);
+
+    const taskIterator = queue[Symbol.asyncIterator]();
+    let { value: task, done } = await taskIterator.next();
+    while (!done) {
+        for (; !done; { value: task, done } = await taskIterator.next()) {
+            await Promise.race(q);
+
+            const t = task;
+            const targetId = task.targetId;
+            const backet = targetId % maxThreads
+            // let backet = qq.findIndex(p => p == targetId);
+
+            // if (backet == -1) {
+            //     backet = qq.findIndex(p => p == -1);
+            // }
+
+
+            q[backet] = q[backet].then(() => {
+                // qq[backet] = t.targetId;
+                return executor.executeTask(t);
+            }).then(() => {
+                // qq[backet] = -1;
+            });
+        }
+
+        await Promise.all(q);
+        ({ value: task, done } = await taskIterator.next());
+    }
 }
